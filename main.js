6f80807bfabbe868557c009d504b204dc4733b47
sriraman5395 <sriraman5395@gmail.com>
2024-05-10 21:58:59 +0530

    Added my project

---
+++ main.js
@@ -0,0 +1,78 @@
+let form = document.getElementById("form");
+let username = document.getElementById("username");
+
+let password = document.getElementById("password");
+
+let btn = document.getElementById("btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validation();
+});
+const setError = (ele, msg) => {
+  let box = ele.parentElement;
+  let error = box.querySelector(".error");
+
+  error.innerText = msg;
+  box.classList.add("error");
+  box.classList.remove("success");
+};
+const setSuccess = (ele) => {
+  let box = ele.parentElement;
+  let error = box.querySelector(".error");
+
+  error.innerText = "";
+  box.classList.add("success");
+  box.classList.remove("error");
+};
+
+const passFormat = (p) => {
+    const re = /^(?=.*\p{Ll})(?=.*\p{Lu})(?=.*[\d|@#$!%*?&])[\p{L}\d@#$!%*?&]{8,96}$/gmu;
+    return re.test(p);
+}
+const userFormat = (u) => {
+    const re = /[^0-9]/;
+    return re.test(u);
+}
+
+function validation() {
+  let user = username.value.trim();
+
+  let pass1 = password.value.trim();
+  
+
+  localStorage.setItem("Username", user);
+
+  localStorage.setItem("Password", pass1);
+ 
+
+  if (user === "") {
+    setError(username, "Username is required");
+  } else if (!userFormat(user)) {
+    setError(username, "Digital are not allowed");
+  } else {
+    setSuccess(username);
+  }
+
+  if (pass1 === "") {
+    setError(password, "Password is required");
+  } else if (!passFormat(pass1)) {
+    setError(password, "Password must be a minimum of 8 characters including number, Upper, Lower And one special character");
+  } else {
+    setSuccess(password);
+  }
+
+  document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+   
+    const searchQuery = `?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+    
+    
+    window.location.replace(`search.html${searchQuery}`);
+  });
+ }
---
+++ resume.js
@@ -0,0 +1,193 @@
+let searchResults = [];
+let currentPage = 0;
+const resultsPerPage = 1;
+function search() {
+    fetch('resume.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch JSON');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+            searchResults = data.resume.filter(resume => resume.basics.AppliedFor.toLowerCase().includes(searchTerm));
+            if (searchResults.length > 0) {
+                currentPage = 0;
+                showResults();
+            } else {
+
+                document.getElementById('name').textContent = 'No results found.';
+                document.getElementById('resumeCard').style.display = 'block';
+                document.getElementById('resumeContainer').style.display = 'none';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+
+        });
+}
+
+
+function showResults() {
+    const start = currentPage * resultsPerPage;
+    const end = (currentPage + 1) * resultsPerPage;
+    const paginatedResults = searchResults.slice(start, end);
+
+    if (paginatedResults.length > 0) {
+        document.getElementById('resumeCard').style.display = 'none';
+        document.getElementById('resumeContainer').style.display = 'block';
+
+        document.getElementById('resumeContainer').innerHTML = '';
+        paginatedResults.forEach(result => populateResume(result));
+
+        document.getElementById('nextButton').style.visibility = (searchResults.length > end) ? 'visible' : 'hidden';
+
+        document.getElementById('backButton').style.visibility = (currentPage > 0) ? 'visible' : 'hidden';
+    } else {
+        document.getElementById('name').textContent = 'No results found.';
+
+        document.getElementById('nextButton').style.visibility = 'hidden';
+        document.getElementById('backButton').style.visibility = 'hidden';
+
+        document.getElementById('resumeCard').style.display = 'block';
+        document.getElementById('resumeContainer').style.display = 'none';
+    }
+}
+
+function nextPage() {
+    currentPage++;
+    showResults();
+}
+function previousPage() {
+    currentPage--;
+    showResults();
+}
+
+
+
+
+function populateResume(data) {
+
+    const resumeCard = document.createElement('div');
+    resumeCard.classList.add('resume-card');
+
+
+
+
+    resumeCard.innerHTML = `
+    
+
+
+        
+        
+    <div class="resume-cardname" id="resumeCard">
+        <div class="applicant-info">
+          <div id="name" class="applicant-name">${data.basics.name}</div>
+          <div id="appliedFor" class="applied-for">${data.basics.AppliedFor}</div>
+        </div>
+        <span class="icon">&#128100;</span>
+      </div>
+      <div class="divide">
+        <div class="left">
+      <div class="technical-skills">
+        <h3 class="head">Personal Information</h3>
+        <ul class="skill-list">
+            <li id="phone" class="skill-item">${data.basics.phone}</li>
+            <li id="email" class="skill-item">${data.basics.email}</li>
+            <li id="profiles" class="skill-item"><div>LinkedIn: <a href="#">linkedin.com/johndoe</a></div></li>
+          </ul>
+      </div>
+
+      <div class="technical-skills">
+        <h3 class="head">Technical Skills</h3>                      
+        <ul id="skillKeywords" class="skill-list">
+        ${data.skills.keywords.map(skill => `<li>${skill}</li>`).join('')}
+    </ul>
+        
+      </div>
+
+     
+
+      <div class="technical-skills">
+        <h3 class="head">Hobbies</h3>
+     
+
+          <ul id="skill-list" class="skill-list">
+          ${data.interests.hobbies.map(hobby => `<li>${hobby}</li>`).join('')}
+      </ul>
+      </div>
+      </div>
+
+
+
+    
+
+     
+<div class="right">
+    <div class="work-experience">
+        <h3>Work Experience</h3>
+        <div class="work-experience-item">
+            <div id="workCompany"><strong>Company Name</strong>:${data.work['Company Name']}</div>
+            <div id="workPosition"><strong>Position</strong>:${data.work.Position}</div>
+            <div id="workStartDate"><strong>Start Date</strong>:${data.work['Start Date']}</div>
+            <div id="workEndDate"><strong>End Date</strong>:${data.work['End Date']}</div>
+            <div id="workSummary"><strong>Company Name</strong>:${data.work.Summary}</div>
+          
+        </div>
+      </div>
+
+      <div class="work-experience">
+        <h3>Projects</h3>
+        <div class="work-experience-item">
+            <div id="projects"><strong>${data.projects.name}</strong> ${data.projects.description} </div>
+      </div>
+
+      <div class="work-experience">
+        <h3>Education</h3>
+        
+            <ul id="educationList">
+                <li id="ug"><strong>UG:</strong>${data.education.UG.institute}</li>
+                <li id="pg"><strong>PG:</strong>${data.education['Senior Secondary'].institute}</li>
+                <li id="highSchool"><strong>High school:</strong>${data.education['High School'].institute}</li>
+              </ul>
+         
+      </div>
+
+      
+      <div class="work-experience">
+        <h3>Internship</h3>
+        
+            <ul id="educationList">
+                <li id="companyName"><strong>Company Name:</strong>${data.Internship['Company Name']}</li>
+                <li id="position"><strong>Position:</strong>${data.Internship.Position}</li>
+                <li id="startDate"><strong>Start Date:</strong>${data.Internship['Start Date']}</li>
+                <li id="endDate"><strong>End Date:</strong>${data.Internship['End Date']}</li>
+                <li id="Summary"><strong>Summary:</strong>${data.Internship.Summary}</li>
+              </ul>
+         
+      </div>
+
+      <div class="work-experience">
+        <h3>Achievements</h3>
+        
+            <ul id="educationList">
+                <li id="Achievements">${data.achievements.Summary}</li>
+              
+              </ul>
+         
+      </div>
+
+     
+   </div>
+  </div>
+  
+    `;
+
+
+    document.getElementById('resumeContainer').appendChild(resumeCard);
+}
+
+
+
+
